a56b0aec6903c3c886fc73ab5e7ae28bf3bb9298
Janis Jansen <janis.jansen@gmx.de>
2022-02-20 12:03:08 +0100

    =added typescript code

---
+++ typescript-tutorial/1/src/main.ts
@@ -0,0 +1,24 @@
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+interface Todo {
+  id: number;
+  label: string;
+  description?: string;
+  done: boolean;
+}
+
+const todos: Todo[] = [
+  { id: 0, label: "Buy groceries", description: "Doritos, Milk, Whisky", done: true },
+  { id: 1, label: "Clean the kitchen", done: false },
+  { id: 2, label: "Subscribe to ROULZ", done: false },
+]
+
+todos.forEach(todo => {
+  const div = document.createElement("div")
+  div.innerHTML = `label: <b>${todo.label}</b> - done: <b>${todo.done}</b> - description: <b>${todo?.description ?? "none"}</b>`;
+  
+
+  app.appendChild(div);
+})
